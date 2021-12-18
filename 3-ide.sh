202e1421fe927ef0761172a2d667ffde2eccf3ec
Leo Jokinen <leo.jokinen81@gmail.com>
2021-12-18 13:45:20 +0200

    Added IDE selection

--- 3-ide.sh
+++ 3-ide.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-echo "Installing VSCode"
+echo '----------------------------------'
+echo 'Choose your IDE: 1=VSCode, 2=IntelliJ IDEA'
+read -p 'Selection: ' CHOICE
+echo '----------------------------------'
+
+if [ $CHOICE == 1 ] 
+then echo "Installing VSCode"
 yay -S --needed --noconfirm visual-studio-code-bin
 
 echo "Installing VSCode extensions"
@@ -10,3 +16,7 @@ code --install-extension --force eamodio.gitlens
 code --install-extension --force mhutchie.git-graph
 code --install-extension --force chuckjonas.apex-pmd
 code --install-extension --force esbenp.prettier-vscode
+elif [ $CHOICE == 2 ] 
+then echo 'Installing IntelliJ IDEA'
+yay -S --needed --noconfirm intellij-idea-community-edition
+fi
