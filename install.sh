fd78424bc264552d08393b5e48008edb17dc87cf
Leo Jokinen <leo.jokinen81@gmail.com>
2021-12-19 23:55:54 +0200

    Install sfdx every time and conditionally add into PATH

--- install.sh
+++ install.sh
@@ -13,16 +13,19 @@ bash 2-packages.sh
 # Install IDE (VSCode or Intellij Idea)
 bash 3-ide.sh
 
-command -v sfdx >/dev/null && echo "SFDX found, skipping install" || {
-  echo "Installing latest stable SFDX"
-  rm -rf ${HOME}/sfdx
-  cd ${TMP_DIR}
-  wget https://developer.salesforce.com/media/salesforce-cli/sfdx/channels/stable/sfdx-linux-x64.tar.xz
-  mkdir ${HOME}/sfdx
-  tar xJf sfdx-linux-x64.tar.xz -C ${HOME}/sfdx --strip-components 1
+echo "Installing latest stable SFDX"
+rm -rf ${HOME}/sfdx
+cd ${TMP_DIR}
+wget https://developer.salesforce.com/media/salesforce-cli/sfdx/channels/stable/sfdx-linux-x64.tar.xz
+mkdir ${HOME}/sfdx
+tar xJf sfdx-linux-x64.tar.xz -C ${HOME}/sfdx --strip-components 1
+# Add sfdx to PATH
+grep "sfdx/bin" ~/.bashrc >/dev/null && echo "sfdx/bin already in PATH, skipping" || {
+  echo "Adding sfdx/bin to PATH at .bashrc"
   echo "PATH=~/sfdx/bin:$PATH" >> ~/.bashrc
+  source ~/.bashrc
 }
 
 rm -rf ${TMP_DIR}
 
-echo -e "\nSuccess, all ready for the development. May the force be with you!"
+echo -e "\nSUCCESS, all ready for coding. May the force be with you!"
