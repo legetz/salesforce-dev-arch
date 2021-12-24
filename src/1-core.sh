d95788b2c1acdfcdd499b98a81059df762d7e012
Leo Jokinen <leo.jokinen81@gmail.com>
2021-12-25 00:30:45 +0200

    Added Docker

--- src/1-core.sh
+++ src/1-core.sh
@@ -6,7 +6,12 @@ PKGS=(
 'nodejs' # Node.Js, latest version
 'jre11-openjdk' # OpenJDK Java 11 runtime
 'yay' # Yet another yogurt. Pacman wrapper and AUR helper
+'docker' # Docker container support
 )
+
+# Activate loop module for Docker
+sudo tee /etc/modules-load.d/loop.conf <<< "loop"
+sudo modprobe loop
 
 # Update pacman package databases
 sudo pacman -Sy --noconfirm
@@ -14,6 +19,10 @@ sudo pacman -Sy --noconfirm
 for PKG in "${PKGS[@]}"; do
     sudo pacman -S --needed --noconfirm $PKG
 done
+
+# Start and enable Docker service. This will start Docker daemon at every boot.
+sudo systemctl start docker.service
+sudo systemctl enable docker.service
 
 # Set default java version
 sudo archlinux-java set java-11-openjdk
