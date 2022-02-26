22c6a7f522bb33321405639019ac20c78074dc2f
Leo Jokinen <leo.jokinen81@gmail.com>
2022-02-26 11:23:48 +0200

    Remove earlier oh my zsh folder

--- src/1-oh-my-zsh.sh
+++ src/1-oh-my-zsh.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Remove possible previous installation
+rm -rf ${HOME}/.oh-my-zsh
+
 # Download and install oh-my-zsh: https://ohmyz.sh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
