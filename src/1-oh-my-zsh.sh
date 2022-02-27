085135dacc29cda3c77446b94ad7f2abc5f21f81
Leo Jokinen <leo.jokinen81@gmail.com>
2022-02-27 22:42:15 +0200

    Change default shell

--- src/1-oh-my-zsh.sh
+++ src/1-oh-my-zsh.sh
@@ -5,3 +5,6 @@ rm -rf ${HOME}/.oh-my-zsh
 
 # Download and install oh-my-zsh: https://ohmyz.sh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Change shell for the next login
+chsh -s $(which zsh)
