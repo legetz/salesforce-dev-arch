723d1ecf744a5123657011a45b0f641877c0341e
Leo Jokinen <leo.jokinen81@gmail.com>
2022-02-28 23:09:57 +0200

    Remove old config backups

--- src/1-oh-my-zsh.sh
+++ src/1-oh-my-zsh.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Remove possible previous installation
+# Remove possible previous installation and config duplicates
 rm -rf ${HOME}/.oh-my-zsh
+rm ${HOME}/.zshrc.pre-oh-my-zsh*
 
 # Download and install oh-my-zsh: https://ohmyz.sh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
